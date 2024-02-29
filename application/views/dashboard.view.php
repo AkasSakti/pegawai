476a604e89eb56e66bfaecaf37779401031f9ff4
AkasSakti <146935384+AkasSakti@users.noreply.github.com>
2024-03-01 01:12:30 +0700

    Update dashboard.view.php

--- application/views/dashboard.view.php
+++ application/views/dashboard.view.php
@@ -1,5 +1,5 @@
 <div class="jumbotron mt-3">
    <h1>Selamat Datang di </h1>
-   <h1 class="display-4">Aplkasi Manajemen Pegawai</h1>
+   <h1 class="display-4">Aplkasi Manajemen Pegawai Fakultas Vokasi Universitas Brawijaya</h1>
    <h3>Anda login sebagai Administrator</h3>
 </div>
