816182559da378474ffe303866b9cd17377a600a
AkasSakti <146935384+AkasSakti@users.noreply.github.com>
2024-03-01 00:53:15 +0700

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+define('DEVELOPMENT_ENVIRONMENT', true);
+define('BASE_PATH', 'http://localhost/pegawai');
+define('DEFAULT_CONTROLLER', 'dashboard');
+
+define('DB_DRIVER', 'mysql');
+define('DB_NAME', 'karyawan');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
